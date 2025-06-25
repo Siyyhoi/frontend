481f93be9df0a49edd41e77da5f8830f8676316c
Pacisho <phatdanaikaeochan@gmail.com>
2025-06-25 11:50:29 +0700

    add carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Carousel from "./components/carousel"
+
 export default function Home() {
   return (
-<div className="flex items-center justify-center h-screen">
-  <h1 className="text-5xl">home page</h1>
-</div>
-
+    <>
+      <Carousel />
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-5xl">home page</h1>
+      </div>
+    </>
   );
 }
